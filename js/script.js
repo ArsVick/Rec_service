80079227a5e9e8139b7fd39be84e267648038e66
ArsVick <archigoy@gmail.com>
2021-10-24 23:22:51 +0300

    Добавил скрипт

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*function viewId(){
+    var first2 = document.getElementById("unique1").value.toString();
+};
+
+butt.onclick = function() {
+    var val = document.getElementById('elem1').value;
+    document.getElementById('str').innerHTML=val.toString();
+};*/
+
 function viewDiv(e){
     e.preventDefault()
     document.getElementById("recommend__book1__name").style.display = "block";
@@ -6,15 +15,20 @@ function viewDiv(e){
     document.getElementById("recommend__book4__name").style.display = "block";
     document.getElementById("recommend__book5__name").style.display = "block";
     document.getElementById("text-area").style.display = "block";
+    /*viewId();*/
+    var first2 = document.getElementById("unique1").value.toString();
+    $.ajax({
+        url: "http://0.0.0.0:8080/lib/"+first2,
+        method: "GET",
+        success: function(resp) {console.log(resp)}
+})
 };
 
-function XX(){
-    var first2 = document.getElementById("user_id").value;
-};
 
 
+/*
 $.ajax({
-    url: "http://0.0.0.0:8080/lib/",
+    url: "http://0.0.0.0:8080/lib/" + first2,
     method: "GET",
     success: function(resp) {console.log(resp)}
-})
+})*/
