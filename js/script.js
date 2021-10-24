2719129c294e75f918aa7039625b1de0894a7f84
ArsVick <archigoy@gmail.com>
2021-10-24 17:25:44 +0300

    Добавил скрипт появления

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function viewDiv(e){
+    e.preventDefault()
+    document.getElementById("recommend__book1__name").style.display = "block";
+    document.getElementById("recommend__book2__name").style.display = "block";
+    document.getElementById("recommend__book3__name").style.display = "block";
+    document.getElementById("recommend__book4__name").style.display = "block";
+    document.getElementById("recommend__book5__name").style.display = "block";
+    document.getElementById("text-area").style.display = "block";
+};
+
